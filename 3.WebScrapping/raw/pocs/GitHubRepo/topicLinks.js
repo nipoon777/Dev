cabe50ff65e10ee1a79848e387c13b2e4508793b
nipoon777 <nipoon.donta7@gmail.com>
2021-03-21 18:29:32 +0530

    github Repo

--- 3.WebScrapping/raw/pocs/GitHubRepo/topicLinks.js
+++ 3.WebScrapping/raw/pocs/GitHubRepo/topicLinks.js
@@ -1,5 +1,11 @@
 let request = require("request");
 let cheerio = require("cheerio");
+let path = require("path");
+let fs = require("fs");
+let PDFDocument = require("pdfkit");
+
+const { fstat } = require("fs");
+
 
 let url = "https://github.com/topics";
 let baseName = "https://github.com/";
@@ -23,6 +29,7 @@ function extractHtml(html){
         let link = selectorTool(topicNames[i]).attr("href");
         let compLink = baseName + link;
         // console.log(compLink);
+        
         getTopicNamesAndRepo(compLink);
     }
 
@@ -31,7 +38,11 @@ function extractHtml(html){
 function getTopicNamesAndRepo(compLink){
     request(compLink, function (err, resp, html ){
         if(err){
-            console.log(err);
+            if(resp.statusCode == 404 ){
+                console.log("Issue Page is not found");
+            }else{
+                console.log(err);
+            }
         }else{
             extractNameURL(html);
         }
@@ -40,18 +51,86 @@ function getTopicNamesAndRepo(compLink){
 
 function extractNameURL(html){
     let selectorTool = cheerio.load(html);
+    
 
-    let topicName = selectorTool(".h1-mktg");
-    console.log(topicName.text());
+    let topicName = selectorTool(".h1-mktg").text();
+    topicName = topicName.trim();
+    
+    createDirectory(topicName);
+    // console.log(topicName.text());
 
     let reposLink = selectorTool("a.text-bold");
 
     for( let i = 0 ; i < 8; i++ ){
         let link = selectorTool(reposLink[i]).attr("href");
         let compLink = baseName + link;
-        console.log(compLink);
+        let fileName = compLink.substring(compLink.lastIndexOf('/') + 1);
+        fileName = fileName.trim();
+        let issueLink = compLink +"/issues";
+        //console.log(issueLink);
+        //console.log(fileName);
+        //createFile(fileName,topicName);
+        makePDF(issueLink,topicName, fileName);
 
     }
-    console.log("```````````````````````````````````````````");
+    // console.log("```````````````````````````````````````````");
+}
 
+function createDirectory(topicName){
+    let folderPath = path.join(__dirname, topicName);
+    
+    if (fs.existsSync(folderPath) == false ){
+        fs.mkdirSync(folderPath);
+    }
+}
+
+function createFile(fileName, topicName){
+    let filePath = path.join(__dirname,topicName, fileName + ".json");
+
+    if (fs.existsSync(filePath) == false ){
+        let createStream = fs.createWriteStream(filePath);
+        createStream.end();
+    }
+}
+
+function makePDF(issueLink, topicName, fileName){
+    request(issueLink , (err,resp,html) => {
+        if(err){
+            if(resp.statusCode == 404 ){
+                console.log("Issue Page Not Found");
+            }else{
+                console.log(err);
+            }
+        }else{
+            extractIssues(html, topicName, fileName);
+        }
+    })
+}
+
+function extractIssues(html, topicName, fileName ){
+    let selectorTool = cheerio.load(html);
+
+    let issues = selectorTool('a[data-hovercard-type="issue"]');
+
+    let issueArr = [];
+    for(let i = 0 ; i < issues.length ; i++ ){
+        let name = selectorTool(issues[i]).text();
+        let link = selectorTool(issues[i]).attr("href");
+        link = baseName + link;
+        issueArr.push({
+            name : name,
+            link : link
+        });
+    }
+    createPdf(issueArr, topicName, fileName);
+}
+
+function createPdf( issueArr, topicName, fileName){
+
+    let filePath = path.join(__dirname, topicName, fileName + ".pdf");
+
+    let pdfDoc = new PDFDocument();
+    pdfDoc.pipe(fs.createWriteStream(filePath));
+    pdfDoc.text(JSON.stringify(issueArr));
+    pdfDoc.end();
 }
