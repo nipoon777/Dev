37652e2b861d62ce223f64ce381171fecb7bff59
nipoon777 <nipoon.donta7@gmail.com>
2021-04-25 18:18:43 +0530

    completed till ticket

--- 7.BrowserToDo/raw/revision/activity/script.js
+++ 7.BrowserToDo/raw/revision/activity/script.js
@@ -18,10 +18,10 @@ colorBtn.forEach(color => {
             </div>
             <div class="color_selector">
                 <!-- <div class="color_filter"> -->
-                    <div class="color pink"></div>
-                    <div class="color blue"></div>
-                    <div class="color green"></div>
-                    <div class="color black"></div>
+                    <div class="color pink #d24d57"></div>
+                    <div class="color blue #00b16a"></div>
+                    <div class="color green #86e2d5"></div>
+                    <div class="color black #24252a"></div>
                 <!-- </div> -->
                 <!-- <div class="color_filter">
                     
@@ -34,7 +34,9 @@ colorBtn.forEach(color => {
             </div>
         </div> */
 
-plusBtn.addEventListener("click", function(){
+plusBtn.addEventListener("click", createTaskBox);
+
+function createTaskBox(){
     console.log("Entered the function");
     let task_box = document.createElement("div");
     task_box.setAttribute("class", "task_box");
@@ -44,14 +46,70 @@ plusBtn.addEventListener("click", function(){
                 placeholder="Enter your text here"></textarea>
             </div>
             <div class="color_selector">
-                    <div class="color pink"></div>
-                    <div class="color blue"></div>
-                    <div class="color green"></div>
-                    <div class="color black"></div>
+                    <div class="color pink #d24d57"></div>
+                    <div class="color blue #00b16a"></div>
+                    <div class="color green #86e2d5"></div>
+                    <div class="color black #24252a"></div>
                     
                 </div>
             </div>
     `;
-
     body.appendChild(task_box);
-});
+
+    //Add event listener to each color
+
+    handleTask(task_box);
+}
+
+function handleTask(task_box){
+    let headColor = "black";
+
+    let taskFilters = document.querySelectorAll(".color");
+
+    taskFilters[3].classList.add("border");
+
+    for( let i = 0 ; i < taskFilters.length ; i++ ){
+        taskFilters[i].addEventListener("click", function(){
+            //Remove border from all elements
+            taskFilters.forEach(taskColor => {
+                taskColor.classList.remove("border");
+            });
+
+            //Add to the selected element
+            taskFilters[i].classList.add("border");
+
+            headColor = taskFilters[i].classList[1];
+            console.log("Current color of task is", headColor);
+        });
+    }
+    let textArea = document.querySelector(".input_box");
+    textArea.addEventListener("keydown", function(e){
+        if(e.key == "Enter" && textArea.value != ""){
+            let content = textArea.value;
+            console.log("Task", textArea.value, "color", headColor);
+            task_box.remove();
+            createTicket(headColor, content);
+        }
+    });
+}
+
+function createTicket(color,content){
+    let ticketContainer = document.createElement("div");
+    ticketContainer.setAttribute("class","ticket_container");
+    ticketContainer.innerHTML =`
+    <div class="ticket_container">
+            <div class="ticket_filter ${color}">
+
+            </div>
+            <div class="ticket_desc_container">
+                <h3 class="uid">#example</h3>
+                <div class="ticket_desc">
+                    ${content}
+                </div>
+            </div>
+
+        </div>
+    `;
+    mainContainer.appendChild(ticketContainer);
+
+}
