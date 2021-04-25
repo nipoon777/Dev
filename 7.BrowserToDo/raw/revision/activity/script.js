75d0b8ccab250bc955add985b5d2a85cd01a1d84
nipoon777 <nipoon.donta7@gmail.com>
2021-04-25 15:31:54 +0530

    Added till task_box

--- 7.BrowserToDo/raw/revision/activity/script.js
+++ 7.BrowserToDo/raw/revision/activity/script.js
@@ -1,5 +1,8 @@
 let colorBtn = document.querySelectorAll(".filter_color");
 let mainContainer = document.querySelector(".main_container");
+let body = document.body;
+
+let plusBtn = document.querySelector(".fa-plus");
 
 colorBtn.forEach(color => {
     color.addEventListener("click", function(e){
@@ -9,11 +12,46 @@ colorBtn.forEach(color => {
     });
 });
 
-/* for(let i = 0 ; i < colorBtn.length ; i++ ){
-    
-    colorBtn[i].addEventListener("click", function(e){
-        console.log("Event", e);
-        let color = colorBtn[i].classList[2];
-        mainContainer.style.backgroundColor = color;
-    });
-} */
+/* <div class="task_box">
+            <div class="input_section">
+                <textarea class ="input_box" placeholder="Enter your text here"></textarea>
+            </div>
+            <div class="color_selector">
+                <!-- <div class="color_filter"> -->
+                    <div class="color pink"></div>
+                    <div class="color blue"></div>
+                    <div class="color green"></div>
+                    <div class="color black"></div>
+                <!-- </div> -->
+                <!-- <div class="color_filter">
+                    
+                </div>
+                <div class="color_filter">
+                    
+                </div>
+                <div class="color_filter">
+                </div> -->
+            </div>
+        </div> */
+
+plusBtn.addEventListener("click", function(){
+    console.log("Entered the function");
+    let task_box = document.createElement("div");
+    task_box.setAttribute("class", "task_box");
+
+    task_box.innerHTML = ` <div class="input_section">
+                <textarea class ="input_box" 
+                placeholder="Enter your text here"></textarea>
+            </div>
+            <div class="color_selector">
+                    <div class="color pink"></div>
+                    <div class="color blue"></div>
+                    <div class="color green"></div>
+                    <div class="color black"></div>
+                    
+                </div>
+            </div>
+    `;
+
+    body.appendChild(task_box);
+});
