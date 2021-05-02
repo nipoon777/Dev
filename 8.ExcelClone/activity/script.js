c3edf81f9380d4657768e5a82017c5f78195a2b7
nipoon777 <nipoon.donta7@gmail.com>
2021-05-02 14:32:31 +0530

    Upto AddressBox

--- 8.ExcelClone/activity/script.js
+++ 8.ExcelClone/activity/script.js
@@ -1,3 +1,28 @@
+let topRow = document.querySelector(".top-row");
+let str = "";
+for (let i = 0; i < 26; i++) {
+    str += `<div class='col'>${String.fromCharCode(65 + i)}</div>`;
+}
+topRow.innerHTML = str;
+let leftCol = document.querySelector(".left-col");
+str = ""
+for (let i = 0; i < 100; i++) {
+    str += `<div class='left-col_box'>${i + 1}</div>`
+}
+leftCol.innerHTML = str;
+
+// 2d array
+let grid = document.querySelector(".grid");
+str = "";
+for (let i = 0; i < 100; i++) {
+    str += `<div class="row">`
+    for (let j = 0; j < 26; j++) {
+        str += `<div class='col' rid = ${i} cid = ${j} contenteditable = "true"></div>`
+    }
+    str += "</div>";
+}
+grid.innerHTML = str;
+
 let addBtn = document.querySelector(".add_container");
 let sheetList = document.querySelector(".sheets_list");
 let firstSheet = document.querySelector(".sheet");
@@ -36,27 +61,25 @@ function handleActiveSheet(e){
     }
 }
 
-let topRow = document.querySelector(".top-row");
-let str = "";
-for (let i = 0; i < 26; i++) {
-    str += `<div class='col'>${String.fromCharCode(65 + i)}</div>`;
-}
-topRow.innerHTML = str;
-let leftCol = document.querySelector(".left-col");
-str = ""
-for (let i = 0; i < 100; i++) {
-    str += `<div class='left-col_box'>${i + 1}</div>`
-}
-leftCol.innerHTML = str;
 
-// 2d array
-let grid = document.querySelector(".grid");
-str = "";
-for (let i = 0; i < 100; i++) {
-    str += `<div class="row">`
-    for (let j = 0; j < 26; j++) {
-        str += `<div class='col'>${String.fromCharCode(65 + j)}${i + 1}</div>`
-    }
-    str += "</div>";
+
+let allCells = document.querySelectorAll(".grid .col");
+let addressBar = document.querySelector(".address_box");
+console.log(allCells);
+allCells[0].click();
+
+
+for(let i = 0 ; i < allCells.length ; i++){
+    allCells[i].addEventListener("click", function handleCells(){
+        let rid = Number(allCells[i].getAttribute("rid"));
+        let cid = Number(allCells[i].getAttribute("cid"));
+    
+        let rowAdd = rid + 1;
+        let colAdd = String.fromCharCode(cid + 65);
+    
+        let address = colAdd + rowAdd;
+        addressBar.value = address;
+    });
 }
-grid.innerHTML = str;
+
+
