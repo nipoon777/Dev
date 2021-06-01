e1485c313cb4eb10553a27af7a0882cf0bd3d277
nipoon777 <nipoon.donta7@gmail.com>
2021-06-01 20:07:50 +0530

    Gallery Styleing And Download

--- 9.Camera/activity/dbScript.js
+++ 9.Camera/activity/dbScript.js
@@ -26,7 +26,7 @@ function addMediaToGallery(data, type){
 }
 
 function viewMedia(){
-    let body = document.querySelector("body");
+    let main_container = document.querySelector(".main_container");
     let tx = db.transaction("gallery", "readonly");
     let gallery = tx.objectStore("gallery");
 
@@ -41,6 +41,7 @@ function viewMedia(){
                 videoContainer.classList.add("gallery_vid_container");
 
                 let video = document.createElement("video");
+                video.classList.add("video_elem");
                 videoContainer.appendChild(video);
 
                 //Delete Button
@@ -55,6 +56,7 @@ function viewMedia(){
                 let downLoadBtn = document.createElement("button");
                 downLoadBtn.classList.add("gallery_download_btn");
                 downLoadBtn.innerText = "Download";
+                downLoadBtn.addEventListener("click", downloadMedia);
 
                 videoContainer.appendChild(deleteBtn);
                 videoContainer.appendChild(downLoadBtn);
@@ -65,7 +67,7 @@ function viewMedia(){
 
                 video.src = URL.createObjectURL(cursor.value.media);
 
-                body.appendChild(videoContainer);
+                main_container.appendChild(videoContainer);
 
             }else{
 
@@ -75,11 +77,14 @@ function viewMedia(){
 
                 let img = document.createElement("img");
                 img.src = cursor.value.media;
+                img.classList.add("img_elem");
                 imgContainer.appendChild(img);
 
                 let downLoadBtn = document.createElement("button");
                 downLoadBtn.classList.add("gallery_download_btn");
                 downLoadBtn.innerText = "Download";
+                downLoadBtn.addEventListener("click", downloadMedia);
+
 
                 let deleteBtn = document.createElement("button");
                 deleteBtn.classList.add("gallery_delete_btn");
@@ -89,7 +94,7 @@ function viewMedia(){
                 imgContainer.appendChild(deleteBtn);
                 imgContainer.appendChild(downLoadBtn);
 
-                body.appendChild(imgContainer);
+                main_container.appendChild(imgContainer);
                 
             }
 
@@ -109,4 +114,41 @@ function deleteBtnHandler(e){
     let mId = e.currentTarget.parentNode.getAttribute('data-mId');
     deleteMediaFromGallery(mId);
     e.currentTarget.parentNode.remove();
+}
+
+
+function downloadMedia(e){
+    let mId = e.currentTarget.parentNode.getAttribute('data-mId');
+    dowmloadMediaFromDb(mId);
+}
+
+function dowmloadMediaFromDb(mId){
+    let tx = db.transaction("gallery", "readonly");
+    let gallery = tx.objectStore("gallery");
+    // console.log(gallery)
+    let req = gallery.openCursor();
+
+    req.onsuccess = function(e){
+        let cursor = req.result;
+        if(cursor){
+            if(cursor.value.mId == mId){
+                console.log(cursor.value);
+                const url = cursor.value.media;
+                let a = document.createElement("a");
+                if( cursor.value.type == "img"){
+                    a.download = mId+".png";
+                    a.href = url;
+                    a.click();
+                }else{
+                    a.download = mId+".mp4";
+                    a.href = url;
+                    a.click();
+                }
+            }else{
+                cursor.continue();
+            }
+        }
+    }
+
+
 }
